aec4ea4bb7b473f06fa7a1d4201ebd5f7193f786
Josemar <josemar.rincon@gmail.com>
2020-03-25 09:38:51 -0300

    sending project to git

---
+++ src/database/migrations/20200324224220_create_ongs.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  // criação da tabela
+  return knex.schema.createTable('ongs', function (table) {
+    table.string('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('whatsapp').notNullable();
+    table.string('city').notNullable();
+    table.string('uf',2).notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  // se der erro volta atrás 
+  return knex.schema.dropTable('ongs');
+
+  
+};
---
+++ src/database/migrations/20200324225214_create_incidents.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+
+  // criação da tabela
+  return knex.schema.createTable('incidents', function (table) {
+    table.increments();
+
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('value').notNullable();
+
+    table.string('ong_id').notNullable();
+    
+    table.foreign('ong_id').references('id').inTable('ongs');
+
+  });
+  
+};
+
+exports.down = function(knex) {
+
+    // se der erro volta atrás 
+    return knex.schema.dropTable('incidents');
+  
+};
